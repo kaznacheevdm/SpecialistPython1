0717fd5e976fe72194354c830c9fae3ac9c4d298
kaznacheevdm <43038529+kaznacheevdm@users.noreply.github.com>
2021-06-30 08:27:47 +0300

    Update 02_hw.py
    Если нужно было через строку, то не совсем понял как через join сделать.

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -6,3 +6,9 @@ import random
 numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
+n=int(input("введите колличество элементов списка: "))
+i=0
+while i<n:
+    numbers.append(random.randint(-100, 100))
+    i+=1
+print (numbers)
