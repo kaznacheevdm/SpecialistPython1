d89d44e3d25f72bb8435e1e7f36bdfe9fce07132
kaznacheevdm <43038529+kaznacheevdm@users.noreply.github.com>
2021-06-30 16:13:31 +0300

    Update add01_task_list.py

--- Module3/practice/list_and_tuple/add01_task_list.py
+++ Module3/practice/list_and_tuple/add01_task_list.py
@@ -3,3 +3,8 @@
 # Получить и вывести на экран список координат y для данного уравнения прямой.
 
 # TODO: your code here
+x = [-2,-1,0,1,2,3,4,5,6,7]
+list_xy=[]
+for i in x:
+    list_xy.append([i,2*i-4])
+print(list_xy)
