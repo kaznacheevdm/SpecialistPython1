0a54514836864d2ce9a0b74cb5636a3d71690322
kaznacheevdm <43038529+kaznacheevdm@users.noreply.github.com>
2021-07-01 14:21:03 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -3,7 +3,33 @@
 # Если по заданным точкам треугольник построить нельзя, выведите соответствующее сообщение.
 # Подсказка: для нахождения площади используйте Теорему Герона
 
-# TODO: your code here
+ef distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return ((x2-x1)**2+(y2-y1)**2)**0.5
 
+def can_triangle(p1, p2, p3):
+    if distance(p1,p2)<distance(p3,p2)+distance(p1,p3):
+        if distance(p3,p2)<distance(p1,p2)+distance(p1,p3):
+            if distance(p1,p3)<distance(p3,p2)+distance(p1,p2):
+                return True
+    return False
+def perimetr(p1, p2, p3):
+    return distance(p1,p2)+distance(p2,p3)+distance(p1,p3)
+ 
+def square(p1,p2,p3):
+    per=perimetr(p1,p2,p3)
+    line_a=distance(p1,p2)
+    line_b=distance(p2,p3)
+    line_c=distance(p1,p3)
+    return (per*(per-line_a)*(per-line_b)*(per-line_c))**0.5
+
+# Пример вызова функции
+pic=((10, 12), (14, 18), (12, 12))
+if(can_triangle(pic)):
+    print(perimetr(pic))
+    print(square(pic))
+else:
+    print("It's no a triangle")
 
 # Не забудьте протестировать вашу функцию
